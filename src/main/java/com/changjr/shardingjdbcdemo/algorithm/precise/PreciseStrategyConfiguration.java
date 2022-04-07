fb2466b8d669e2dacf14cb9823def5109bc42a41
changjunrui3 <changjunrui3@jd.com>
2022-04-07 18:52:48 +0800

    style

--- src/main/java/com/changjr/shardingjdbcdemo/algorithm/precise/PreciseStrategyConfiguration.java
+++ src/main/java/com/changjr/shardingjdbcdemo/algorithm/precise/PreciseStrategyConfiguration.java
@@ -11,18 +11,11 @@ import java.util.Collection;
 import java.util.Date;
 
 public class PreciseStrategyConfiguration implements PreciseShardingAlgorithm<Integer> {
-
   @Override
-  public String doSharding(
-      Collection<String> collection, PreciseShardingValue<Integer> preciseShardingValue) {
+  public String doSharding(Collection<String> collection, PreciseShardingValue<Integer> preciseShardingValue) {
     System.out.println("精准匹配策略：PreciseStrategyConfiguration");
 
-    /**
-     * tableNames 对应分片库中所有分片表的集合 shardingValue 为分片属性，其中 logicTableName 为逻辑表，columnName 分片健（字段），value
-     * 为从 SQL 中解析出的分片健的值
-     */
     for (String tableName : collection) {
-      /** 取模算法，分片健 % 表数量 */
       Integer value = preciseShardingValue.getValue() % 2 + 1;
       if (tableName.endsWith(String.valueOf(value))) {
         return tableName;
